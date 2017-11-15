208daffb79c9e3c1d381a359d4902ba1342a3fbb
Cristóbal Castillo <1154738+c-castillo@users.noreply.github.com>
2017-11-15 09:37:36 -0300

    Update vscode.plugin.zsh

--- vscode.plugin.zsh
+++ vscode.plugin.zsh
@@ -8,10 +8,10 @@ alias vscode='code'
 function code {
     if [[ $# = 0 ]]
     then
-        open_command -a "Visual Studio Code"
+        open -a "Visual Studio Code"
     else
         local argPath="$1"
         [[ $1 = /* ]] && argPath="$1" || argPath="$PWD/${1#./}"
-        open_command -a "Visual Studio Code" "$argPath"
+        open -a "Visual Studio Code" "$argPath"
     fi
 }
